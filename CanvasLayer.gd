57bb49170a9b5fdd8a880688c0be82e06ed7c5e2
unknown <nintendoemailthing@yahoo.com>
2024-05-23 15:43:20 -0700

    penis button still doesnt work

--- CanvasLayer.gd
+++ CanvasLayer.gd
@@ -7,12 +7,16 @@ func _ready():
 
 func _on_button_pressed():
 	print("PENIS")
-	$HTTPRequest.request("http://www.mocky.io/v2/5185415ba171ea3a00704eed")	
+	var error = $HTTPRequest.request("http://www.mocky.io/v2/5185415ba171ea3a00704eed")	
 
 func _on_http_request_request_completed(result, response_code, headers, body):
+	print(result==HTTPRequest.RESULT_SUCCESS)
+	print(response_code)
+	print(headers)
+	print(body)
 	var json = JSON.new()
 	var json_string = json.parse(body.get_string_from_utf8())
-	print(typeof(json_string))
+	print(json_string == OK)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
